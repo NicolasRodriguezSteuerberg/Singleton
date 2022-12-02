ee79bb83a18e3aade63110e516fd9c8f4e1edbe0
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-12-02 09:23:03 +0100

    singlenton

---
+++ src/BaseDeDatos.java
@@ -0,0 +1,48 @@
+
+public class BaseDeDatos {
+    // atributos para comprobar que siempre es el mismo objeto
+    private String nombre;
+    private String token;
+
+    // esta va a ser nuestra unica instancia
+    private static BaseDeDatos instance = null;
+
+    /**
+     * Constructor bloqueado con private
+     */
+    private BaseDeDatos() {
+    }
+
+    /**
+     * Segun exista o no ya el objeto lo instanciamos o cambiamos los datos
+     *
+     * @return la instancia. Si no existe la crea primero
+     */
+    public static BaseDeDatos getInstance() {
+        // compruebo si ya está creada
+        if (instance == null) {
+            // como no está creada, la creo
+            // desde aqui si que puedo acceder al constructor
+            // porque estoy en la misma clase
+            instance = new BaseDeDatos();
+        }
+        // devuelvo la instancia
+        return instance;
+    }
+
+    /**
+     * Edad de la persona
+     * @param token de la persona
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * Nombre de la persona
+     * @param nombre de la persona
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto para aprender a hacer un Singleton");
+
+        // intento instanciar un objeto con el constructor privado
+        // esto es un error
+        // Singleton miUnicaInstancia = new Singleton();
+
+        // creamos un objeto
+        BaseDeDatos miUnicaInstancia = BaseDeDatos.getInstance();
+        // rellenamos los atributos
+        miUnicaInstancia.setToken("AAR3");
+        miUnicaInstancia.setNombre("Pepe");
+
+        // creamos otro objeto Singleton
+        // como la única manera es con getInstance(), este método se encargará
+        // de devolvernos el objeto creado anteriormente
+        BaseDeDatos otraInstancia = BaseDeDatos.getInstance();
+        // podemos cambiar la edad
+        // comprobar con el debug que los dos objetos:
+        // miUnicaInstancia y otraInstanciaSeraLaMisma son el mismo
+        otraInstancia.setToken("BG4H");
+
+        // version 1.1 del programa con otra instancia
+        BaseDeDatos versionInstancia = BaseDeDatos.getInstance();
+        versionInstancia.setNombre("version 1.2");
+    }
+}
